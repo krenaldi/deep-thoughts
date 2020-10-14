dfdb57026a0b7422cb1b63ac409efff7245c4ca8
Kris Renaldi <krenaldi@comcast.net>
2020-10-14 18:00:24 -0400

    Installed Apollo Server and setup GraphQL Schmeas

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,11 @@
+// import the graphql tagged template function
+const { gql } = require('apollo-server-express');
+
+// create typeDefs
+const typeDefs = gql`
+    type Query {
+        helloWorld: String
+    }`;
+
+// export typeDefs
+module.exports = typeDefs;
