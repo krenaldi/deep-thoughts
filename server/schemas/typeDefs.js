1ec592b7137ee18eab71d4e40b395a5f2aca3cc0
Kris Renaldi <krenaldi@comcast.net>
2020-10-16 14:37:46 -0400

    Added mutations to typeDefs and resolvers

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -32,7 +32,13 @@ const typeDefs = gql`
         user(username: String!): User
         thoughts(username: String): [Thought]
         thought(_id: ID!): Thought
-    }`;
+    }
+    type Mutation {
+        login(email: String!, password: String!): User
+        addUser(username: String!, email: String!, password: String!): User
+    }
+    
+    `;
 
 // export typeDefs
 module.exports = typeDefs;
